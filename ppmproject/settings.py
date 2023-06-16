ed0f6c370ca086122d563069e6daba8b055926ed
luke1002 <lucalascialfari2001@gmail.com>
2023-06-16 16:38:49 +0200

    Added allowed hosts

--- ppmproject/settings.py
+++ ppmproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-um%!ld5k%s%bd4+$(=)h3!#ydg9+n5!lwk%wr@czh6!^zy5g66'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+
+    #'django-insecure-um%!ld5k%s%bd4+$(=)h3!#ydg9+n5!lwk%wr@czh6!^zy5g66'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['.onrender.com']
-
+ALLOWED_HOSTS = []
 
 # Application definition
 
